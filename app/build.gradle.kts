7d639e93790c7c0ac4c183b3a367b1731c81ca23
Staniislaw <stas.vip.07@mail.ru>
2024-12-11 23:04:22 +0200

    #fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+
+
 }
 
 dependencies {
@@ -40,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
 }
