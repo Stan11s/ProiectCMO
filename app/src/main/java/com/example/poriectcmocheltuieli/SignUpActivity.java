7d10abecf43603b9d0f07ca3549175765d1e3cb5
Staniislaw <stas.vip.07@mail.ru>
2025-01-09 09:23:15 +0200

    #adaugare erori corespunzatoare la creare nou user

--- app/src/main/java/com/example/poriectcmocheltuieli/SignUpActivity.java
+++ app/src/main/java/com/example/poriectcmocheltuieli/SignUpActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -65,16 +66,38 @@ public class SignUpActivity extends AppCompatActivity {
             mAuth.createUserWithEmailAndPassword(username, password)
                     .addOnCompleteListener(SignUpActivity.this, task -> {
                         if (task.isSuccessful()) {
-                            // Înregistrare reusita, navigam la LoginActivity
+                            // inregistrare reusita, navigam la MainActivity
                             Toast.makeText(SignUpActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
                             startActivity(intent);
-                            finish();  // Închidem activitatea de înregistrare
+                            finish(); // Închidem activitatea de înregistrare
                         } else {
                             // Eroare la înregistrare
-                            Toast.makeText(SignUpActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                            if (task.getException() instanceof FirebaseAuthException) {
+                                FirebaseAuthException exception = (FirebaseAuthException) task.getException();
+                                String errorCode = exception.getErrorCode();
+
+                                // Verificam codul de eroare
+                                switch (errorCode) {
+                                    case "ERROR_EMAIL_ALREADY_IN_USE":
+                                        Toast.makeText(SignUpActivity.this, "Acest user deja exista!", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case "ERROR_WEAK_PASSWORD":
+                                        Toast.makeText(SignUpActivity.this, "Parola este prea slabă!", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case "ERROR_INVALID_EMAIL":
+                                        Toast.makeText(SignUpActivity.this, "Adresa de email este invalida!", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    default:
+                                        Toast.makeText(SignUpActivity.this, "Authentication failed: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                            }
                         }
                     });
+
         });
     }
 }
